410c542658d91cf4dccc646bf828c300d604b886
TimKorAnd <timkorand@gmail.com>
2020-07-07 06:59:48 +0300

    task complete. review 1

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     declare(strict_types=1);
     date_default_timezone_set('Europe/Uzhgorod');
 
-    define('COOKIE_LIFETIME', time() + (60*60*24*120)); //roughly termin of internature
+    define('COOKIE_LIFETIME', time() + (60*60*24*120)); //roughly termin of internship
     $userFullName = '';
 
     //if user reg form submitted
@@ -10,7 +10,6 @@
         $userFullName  = $_SESSION['registeredUserId']['userFullName'] = $_POST['userFirstName'].' '.$_POST['userLastName'];
         $userAge = $_SESSION['registeredUserId']['userAge'] = $_POST['userAge'];
         $sessionName = $_SESSION['registeredUserId']['sessionName'] = hash('sha256', $userFullName.$userAge);
-        //$_SESSION['registeredUserId']['count'] = $_COOKIE[$sessionName]['count'] ?? 0;
     endif;
 
     $sessionName = $_SESSION['registeredUserId']['sessionName'] ?? 'unregisteredUserSession';
@@ -42,7 +41,6 @@
 
     /**
      *
-     * @param int $visitCounter
      * @param string $strCount
      * @return bool
      */
@@ -62,27 +60,89 @@
     }
 
 ?>
-<div class="sessionCount">
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>ONYX Intership Test Task</title>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="test task for ONYX internship by timkorand@gmail">
+    <link rel="stylesheet"
+          href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
+          integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk"
+          crossorigin="anonymous">
+    <style>
+        input {
+            display: block;
+            margin-bottom: 20px;
+            width: 25em;
+        }
+
+        input:invalid:required {
+            box-shadow: 0 0 5px 1px red;
+        }
+
+        input:valid {
+            box-shadow: 0 0 5px 1px green;
+        }
+
+        .times {
+            color: red;
+            font-weight: bold;
+        }
+        .dateVisit {
+            color: green;
+            font-weight: lighter;
+        }
+    </style>
+</head>
+
+<body data-gr-c-s-loaded="true" class="">
+<div class="sessionCount container">
     <?php if (!empty($userFullName)):?>
         <p>Вы <?=$userFullName?> и вы родились в
-            <?=($date->sub(new DateInterval("P".$userAge."Y"))->format("Y"))?> году.</p>
+            <?php try {
+                echo ($date->sub(new DateInterval("P" . $userAge . "Y"))->format("Y"));
+            } catch (Exception $e) {
+            } ?> году.</p>
     <?php endif;?>
-    <p>Вы здесь уже <?=$visitCounter?> раз<?=getEnding($visitCounter)?>.</p>
+    <p>Вы здесь уже <span class="times"><?=$visitCounter?></span> раз<?=getEnding($visitCounter)?>.</p>
     <?php if (!empty($lastVisitTime)):?>
-        <p>Ваш последний визит был <?=$lastVisitTime?> .</p>
+        <p>Ваш последний визит был <span class="dateVisit"><?=$lastVisitTime?></span>.</p>
     <?php endif;?>
 
+    <form id="userRegistration" action = "index.php" method="post">
+        <fieldset>
+            <legend>Форма для идентификации:</legend>
+            <label for="userFirstName">Введите имя:</label>
+            <input id="userFirstName"
+                   name = "userFirstName"
+                   type="text"
+                   value = ""
+                   pattern="[a-zA-Zа-яА-Я]*"
+                   placeholder="только буквы, минимально одна"
+                   autofocus
+                   required>
+            <label for="userLastName">Введите фамилию:</label>
+            <input id = "userLastName"
+                   name = "userLastName"
+                   type="text"
+                   value = ""
+                   pattern="[a-zA-Zа-яА-Я]*"
+                   placeholder="только буквы, минимально одна"
+                   required>
+            <label for="userAge">Введите возраст:</label>
+            <input id = "userAge"
+                   name = "userAge"
+                   type="number"
+                   min="0"
+                   placeholder="только цифры, минимум одна" required>
+            <input name = "userRegistrationSubmit" type="submit" value = "Идентифицировать">
+        </fieldset>
+    </form>
 </div>
-<form id="userRegistration" action = "index.php" method="post">
-    <fieldset>
-        <legend>Форма для идентификации:</legend>
-        <label for="userFirstName">Введите имя:</label>
-        <input id="userFirstName" name = "userFirstName" type="text" value = "" required>
-        <label for="userLastName">Введите фамилию:</label>
-        <input id = "userLastName" name = "userLastName" type="text" value = "" required>
-        <label for="userAge">Введите возраст:</label>
-        <input id = "userAge" name = "userAge" type="number" min="0" required>
-        <input name = "userRegistrationSubmit" type="submit" value = "Submit">
-    </fieldset>
-</form>
+</body>
+</html>
 
