9e4c7bbc2746948342e214161d7f76b55046e050
TimKorAnd <timkorand@gmail.com>
2020-07-07 05:21:52 +0300

    logical functional  complete

--- index.php
+++ index.php
@@ -1,12 +1,16 @@
 <?php
     declare(strict_types=1);
-    //$sessionName = 'unregisteredUserSession';
+    date_default_timezone_set('Europe/Uzhgorod');
+
+    define('COOKIE_LIFETIME', time() + (60*60*24*120)); //roughly termin of internature
+    $userFullName = '';
+
     //if user reg form submitted
     if (isset($_POST['userRegistrationSubmit'])):
         $userFullName  = $_SESSION['registeredUserId']['userFullName'] = $_POST['userFirstName'].' '.$_POST['userLastName'];
         $userAge = $_SESSION['registeredUserId']['userAge'] = $_POST['userAge'];
         $sessionName = $_SESSION['registeredUserId']['sessionName'] = hash('sha256', $userFullName.$userAge);
-        $_SESSION['registeredUserId']['count'] = $_COOKIE[$sessionName.'count'] ?? 0;
+        //$_SESSION['registeredUserId']['count'] = $_COOKIE[$sessionName]['count'] ?? 0;
     endif;
 
     $sessionName = $_SESSION['registeredUserId']['sessionName'] ?? 'unregisteredUserSession';
@@ -14,26 +18,71 @@
     session_name($sessionName);
     session_start();
 
-    $count = $_COOKIE[$sessionName.'count'] ?? 0;
-    setcookie($sessionName.'count',(String)(++$count),time() + (60*60*24*120) ); //about termins of internature
+    $visitCounter = $_COOKIE[$sessionName]['count'] ?? 0;
+    $lastVisitTime = $_COOKIE[$sessionName]['lastVisitTime'] ?? '';
+    $date = new DateTime();
+    setcookie($sessionName."[count]",(string)(++$visitCounter), COOKIE_LIFETIME);
+    setcookie($sessionName."[lastVisitTime]",$date->format("d/m/Y H:i:s"), COOKIE_LIFETIME);
 
 
-?>    
-<div class="sessionCount">
-    <p>session counter for <?=$userFullName ?? ''?>:</p>
-    <?php
-       /* if (!isset($_SESSION['registeredUserId']['count'])) {
-            $_SESSION['registeredUserId']['count'] = 0;
+    /**
+     * @param int $visitCounter quantity of visits
+     * @return string 'a' for "раза" or '' for "раз"
+     */
+    function getEnding(int $visitCounter): string
+    {
+        $strCount = (string)$visitCounter;
+        if (isLastDigitCausingEndingOnA($strCount)) {
+            if (($visitCounter < 5) || !isLastCausingNumberInSecondTen($strCount) ) {
+                return 'а';
+            }
         }
-        echo ++$_SESSION['registeredUserId']['count'];*/
-        echo $count;
+        return '';
+    }
 
-    ?>
+    /**
+     *
+     * @param int $visitCounter
+     * @param string $strCount
+     * @return bool
+     */
+    function isLastCausingNumberInSecondTen(string $strCount): bool
+    {
+        return (substr($strCount, -2, 1) == '1');
+    }
+
+    /**
+     * @param string $strCount
+     * @return bool
+     */
+    function isLastDigitCausingEndingOnA(string $strCount): bool
+    {
+        define('LINE_OF_DIGITS_ENDING_FOR_A', '234');
+        return strpos(LINE_OF_DIGITS_ENDING_FOR_A, substr($strCount, -1)) !== false;
+    }
+
+?>
+<div class="sessionCount">
+    <?php if (!empty($userFullName)):?>
+        <p>Вы <?=$userFullName?> и вы родились в
+            <?=($date->sub(new DateInterval("P".$userAge."Y"))->format("Y"))?> году.</p>
+    <?php endif;?>
+    <p>Вы здесь уже <?=$visitCounter?> раз<?=getEnding($visitCounter)?>.</p>
+    <?php if (!empty($lastVisitTime)):?>
+        <p>Ваш последний визит был <?=$lastVisitTime?> .</p>
+    <?php endif;?>
+
 </div>
 <form id="userRegistration" action = "index.php" method="post">
-    <input name = "userFirstName" type="text" value = "">
-    <input name = "userLastName" type="text" value = "">
-    <input name = "userAge" type="number">
-    <input name = "userRegistrationSubmit" type="submit" value = "Submit">
+    <fieldset>
+        <legend>Форма для идентификации:</legend>
+        <label for="userFirstName">Введите имя:</label>
+        <input id="userFirstName" name = "userFirstName" type="text" value = "" required>
+        <label for="userLastName">Введите фамилию:</label>
+        <input id = "userLastName" name = "userLastName" type="text" value = "" required>
+        <label for="userAge">Введите возраст:</label>
+        <input id = "userAge" name = "userAge" type="number" min="0" required>
+        <input name = "userRegistrationSubmit" type="submit" value = "Submit">
+    </fieldset>
 </form>
 
